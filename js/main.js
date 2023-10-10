a43fea5e50025197731a3d621d50a6a72351661f
Max <max.assermark@gmail.com>
2023-10-10 11:18:16 +0200

    Added Add-to-cart feature and test code for hiding inspect element

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ const inspectImage = document.getElementById("inspect-image");
 const priceElement = document.getElementById("price");
 const descriptionElement = document.getElementById("description");
 
+const scAmountElement = document.getElementById("shopping-cart-amount");
+const ATCElement = document.getElementById("add-to-cart");
+
+window.onload = init;
+
+function init() {
+    updateShoppingCart();
+}
+
 Array.from(itemElements).forEach(element => element.addEventListener("click", inspectItem));
 
 function inspectItem(event) {
@@ -21,4 +30,37 @@ function inspectItem(event) {
     priceElement.innerHTML = item.price;
     descriptionElement.innerHTML = item.text;
     inspectBox.style.display = "block";
+
+    ATCElement.addEventListener("click", function() {
+        addToCart(itemID);
+    });
+
+/*     window.addEventListener("click", function(e) {
+        if (inspectBox.contains(e.target)) {
+            console.log("Clicked in inspectBox");
+        } else {
+            inspectBox.style.display = "none";
+            console.log("Clicked outside of inspectBox")
+        }
+    }); */
+}
+
+function updateShoppingCart() {
+    let cart = JSON.parse(localStorage.getItem("cart")??"[]");
+
+    if (cart.length === 0) {
+        return
+    } else {
+        scAmountElement.style.display = "block";
+        scAmountElement.innerHTML = cart.length;
+    }
+}
+
+function addToCart(itemID) {
+    let cart = JSON.parse(localStorage.getItem("cart")??"[]");
+    cart.push(itemID);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    updateShoppingCart();
 }
